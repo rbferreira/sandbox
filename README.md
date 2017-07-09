60df314582cf67286034aadad76c417699204d97
RafaelSiri <rferreira.suporte@gmail.com>
2017-07-09 03:38:22 -0300

    commit 1

---
+++ README.md
@@ -0,0 +1,2 @@
+# helloworld
+Leia-me... sqn
---
+++ teste.py
@@ -0,0 +1,19 @@
+print("Testando o python sozinho...")
+
+print("Testando o valor de duas variaveis")
+
+var1 = input('Insira a primeira variavel:')
+
+var2 = input('Insira a segunda variavel:')
+
+print('A variavel 1 é:', var1)
+print('A variavel 2 e:', var2)
+
+def condicao(): 
+    if var1 > var2:
+        print (var1, "e maior que", var2)
+    else:
+        print('FODA-SE')
+
+
+condicao()
